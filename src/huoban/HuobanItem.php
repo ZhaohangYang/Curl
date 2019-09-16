f34b34e1b38291a8fa75565bb7fbd2fb201721ab
zhaohangyang <zhaohangyang@qq.com>
2019-09-16 10:33:28 +0800

    init

---
+++ src/huoban/HuobanItem.php
@@ -0,0 +1,10 @@
+<?php
+namespace Huoban;
+
+class HuobanItem
+{
+    public function sayHellow()
+    {
+        echo 'hellow';
+    }
+}
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . "/vendor/autoload.php";
+
+use Huoban\HuobanItem;
+
+HuobanItem::sayHellow();
